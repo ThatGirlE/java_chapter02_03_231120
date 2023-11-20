fe9d8f2d0f4a5ee819048008152b68f2e8b85304
ThatGirlE <dandyjun88@hanmail.net>
2023-11-20 10:52:21 +0900

    자바 git

---
+++ src/typetest/Test01.java
@@ -0,0 +1,14 @@
+package typetest;
+
+public class Test01 {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 3;
+		double result;
+		
+		result = (double)a / b; // 정수와 정수의 나눗셈에서 실수연산 결과를 얻으려면 두 정수 중의 하나이상은 반드시 실수여야함
+		// 10 / 3.0 or 10.0 / 3
+		System.out.println(result);
+		
+	}
+}
---
+++ src/typetest/Test02.java
@@ -0,0 +1,24 @@
+package typetest;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		
+		String str1 = "123";
+		String str2 = "123.123"; // 소수점이 들어간 문자열은 정수로 변환 불가
+		int itg = Integer.parseInt(str1);  // 문자열을 정수로 변환
+		
+		double dbl = Double.parseDouble(str2); // 문자열을 실수로 변환
+		
+		System.out.println(itg);
+		System.out.println(dbl);
+		
+		int i1 = 1234;
+		
+		String str3 = String.valueOf(i1); // 정수 또는 실수를 문자열로 변환
+		
+		System.out.println(str3);
+		
+	}
+
+}
